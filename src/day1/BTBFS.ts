ff0ef77995838e10546375588072f6b60203144b
Jared Wyce <wycejared@gmail.com>
2023-09-27 20:30:55 -0400

    🌲 tree traversals

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,16 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const node = q.shift() as BinaryNode<number>;
+
+        if (node.value === needle) {
+            return true;
+        }
+
+        if (node.left) q.unshift(node.left);
+        if (node.right) q.unshift(node.right);
+    }
+
+    return false;
 }
